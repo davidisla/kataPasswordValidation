f8ce54dac44db04bd8ed5092a258edcdde83d03d
David Isla <d.isla@ibermatica.com>
2022-08-10 22:35:11 +0200

    REFACTOR: Pre-step to introduce a Builder/Mother to generate valid/invalid passwords

--- src/test/kotlin/PasswordValidationTest.kt
+++ src/test/kotlin/PasswordValidationTest.kt
@@ -1,21 +1,32 @@
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 class PasswordValidationTest {
+
+    val NEUTRAL = "/"
+    val UPPERCASE = "A"
+    val LOWERCASE = "b"
+    val UNDERSCORE = "_"
+    val NUMBER = "1"
+
+    val PWD_4_CHARACTERS = UPPERCASE + LOWERCASE + NUMBER + UNDERSCORE
+    val PWD_8_CHARACTERS = UPPERCASE + LOWERCASE + NUMBER + UNDERSCORE + NEUTRAL.repeat(4)
+    val PWD_WITHOUT_UPPERCASE = NEUTRAL + LOWERCASE + NUMBER + UNDERSCORE + NEUTRAL.repeat(5)
 
     @Nested
     inner class InvalidPasswords {
 
         @Test
         fun `should have more than 8 characters`() {
-            assertFalse( PasswordPattern.validation("Ab1_") )
-            assertFalse( PasswordPattern.validation("Ab1_5678") )
+            assertFalse(PasswordPattern.validation(PWD_4_CHARACTERS))
+            assertFalse(PasswordPattern.validation(PWD_8_CHARACTERS))
         }
 
         @Test
         fun `should have a capital letter`() {
-            assertFalse( PasswordPattern.validation( "_b1_56789"))
+            assertFalse(PasswordPattern.validation(PWD_WITHOUT_UPPERCASE))
         }
     }
+
 }
