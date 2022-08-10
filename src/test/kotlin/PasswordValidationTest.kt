bf78252340a8a0fd9cfff62d6a5c8766d8e58114
David Isla <d.isla@ibermatica.com>
2022-08-10 20:08:39 +0200

    RED: Passwords should have more than 8 characters

--- src/test/kotlin/PasswordValidationTest.kt
+++ src/test/kotlin/PasswordValidationTest.kt
@@ -1,10 +1,15 @@
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 class PasswordValidationTest {
 
-    @Test
-    fun firstTest() {
-        assertEquals(1,2)
+    @Nested
+    inner class InvalidPasswords {
+
+        @Test
+        fun `should have more than 8 characters`() {
+            assertFalse( PasswordPattern.validation("Ab1_") )
+        }
     }
 }
