602ce89aa9bd78627bde4f2af5533cd5dee79839
David Isla <d.isla@ibermatica.com>
2022-08-11 17:54:13 +0200

    REFACTOR: New interface created. I'm going to move the logic of validations to classes as previous pass to make a Builder

---
+++ src/main/kotlin/Validation.kt
@@ -0,0 +1,28 @@
+interface Validation {
+
+    /**
+     * Interesting decision here.
+     *
+     * You can make a regular strategy approach and ends up using this interface
+     * overriding a method with a name very similar to the interface, something
+     * like this:
+     *
+     *      interface Validation
+     *          fun validate(pattern: String): Boolean
+     *
+     *      class LengthValidation(val length: Int): Validation
+     *
+     *      val lengthValidation = LengthValidation(16)
+     *
+     *      val isValid = lengthValidation.validate(password)
+     *
+     * To avoid this, I've decided to override the `invoke` operator, looking
+     * for this:
+     *
+     *      val lengthValidation = LengthValidation(16)
+     *
+     *      val isValid = lengthValidation(password)
+     *
+     */
+    operator fun invoke(pattern: String): Boolean
+}
