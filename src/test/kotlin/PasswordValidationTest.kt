f2d0a8ecac0790f340aff18fea72683defd3ec10
David Isla <d.isla@ibermatica.com>
2022-08-11 19:30:54 +0200

    REFACTOR: Reorganized variables, tests, etc.

--- src/test/kotlin/PasswordValidationTest.kt
+++ src/test/kotlin/PasswordValidationTest.kt
@@ -16,46 +16,53 @@ class PasswordValidationTest {
     val FILLER4 = NEUTRAL.repeat(4)
     val FILLER8 = NEUTRAL.repeat(8)
 
-    val validPassword = mutableListOf(UPPERCASE, LOWERCASE, NUMBER, UNDERSCORE, FILLER8)
-
-    val PWD_VALID_PASS = validPassword.join()
-    val PWD_4_CHARACTERS = validPassword.without(FILLER8).join()
-    val PWD_8_CHARACTERS = validPassword.without(FILLER8).plus(FILLER4).join()
-    val PWD_WITHOUT_UPPERCASE = validPassword.without(UPPERCASE).join()
-    val PWD_WITHOUT_LOWERCASE = validPassword.without(LOWERCASE).join()
-    val PWD_WITHOUT_NUMBER = validPassword.without(NUMBER).join()
-    val PWD_WITHOUT_UNDERSCORE = validPassword.without(UNDERSCORE).join()
-
     @Nested
     inner class Iteration1 {
+        val validPassword = mutableListOf(UPPERCASE, LOWERCASE, NUMBER, UNDERSCORE, FILLER8)
+
+        val PWD_VALID_PASS = validPassword.join()
+        val PWD_4_CHARACTERS = validPassword.without(FILLER8).join()
+        val PWD_8_CHARACTERS = validPassword.without(FILLER8).plus(FILLER4).join()
+        val PWD_WITHOUT_UPPERCASE = validPassword.without(UPPERCASE).join()
+        val PWD_WITHOUT_LOWERCASE = validPassword.without(LOWERCASE).join()
+        val PWD_WITHOUT_NUMBER = validPassword.without(NUMBER).join()
+        val PWD_WITHOUT_UNDERSCORE = validPassword.without(UNDERSCORE).join()
+
+        val passwordPattern = PasswordPattern.Builder()
+            .withMinimumLength(9)
+            .withUppercase()
+            .withLowercase()
+            .withDigit()
+            .withUnderscore()
+            .build()
 
         @Nested
         inner class InvalidPasswords {
 
             @Test
             fun `should have more than 8 characters`() {
-                assertFalse(PasswordPattern.validation(PWD_4_CHARACTERS))
-                assertFalse(PasswordPattern.validation(PWD_8_CHARACTERS))
+                assertFalse(passwordPattern.validate(PWD_4_CHARACTERS))
+                assertFalse(passwordPattern.validate(PWD_8_CHARACTERS))
             }
 
             @Test
             fun `should have a capital letter`() {
-                assertFalse(PasswordPattern.validation(PWD_WITHOUT_UPPERCASE))
+                assertFalse(passwordPattern.validate(PWD_WITHOUT_UPPERCASE))
             }
 
             @Test
             fun `should have a lowercase letter`() {
-                assertFalse(PasswordPattern.validation(PWD_WITHOUT_LOWERCASE))
+                assertFalse(passwordPattern.validate(PWD_WITHOUT_LOWERCASE))
             }
 
             @Test
             fun `should have a number`() {
-                assertFalse(PasswordPattern.validation(PWD_WITHOUT_NUMBER))
+                assertFalse(passwordPattern.validate(PWD_WITHOUT_NUMBER))
             }
 
             @Test
             fun `should have an underscore`() {
-                assertFalse(PasswordPattern.validation(PWD_WITHOUT_UNDERSCORE))
+                assertFalse(passwordPattern.validate(PWD_WITHOUT_UNDERSCORE))
             }
         }
 
@@ -64,7 +71,7 @@ class PasswordValidationTest {
 
             @Test
             fun `should be valid with upper + lower + number + underscore and more than 8 length`() {
-                assertTrue(PasswordPattern.validation(PWD_VALID_PASS))
+                assertTrue(passwordPattern.validate(PWD_VALID_PASS))
             }
         }
     }
@@ -74,6 +81,8 @@ class PasswordValidationTest {
 
         @Test
         fun `check iteration 1 pattern`() {
+            val password = UPPERCASE + LOWERCASE + NUMBER + UNDERSCORE + NEUTRAL.repeat(5)
+
             val passwordPattern = PasswordPattern.Builder()
                 .withMinimumLength(9)
                 .withUppercase()
@@ -82,12 +91,11 @@ class PasswordValidationTest {
                 .withUnderscore()
                 .build()
 
-            assertTrue(passwordPattern.validate(PWD_VALID_PASS))
+            assertTrue(passwordPattern.validate(password))
         }
 
         @Test
         fun `Validation 2 should valid with uppercase, lowercase, digit and more than 6 characters`() {
-
             val password = UPPERCASE + LOWERCASE + NUMBER + NEUTRAL.repeat(4)
 
             val passwordPattern = PasswordPattern.Builder()
@@ -102,7 +110,6 @@ class PasswordValidationTest {
 
         @Test
         fun `Validation 3 should valid with uppercase, lowercase, digit and more than 16 characters`() {
-
             val password = UPPERCASE + LOWERCASE + UNDERSCORE + NEUTRAL.repeat(14)
 
             val passwordPattern = PasswordPattern.Builder()
@@ -116,5 +123,4 @@ class PasswordValidationTest {
         }
 
     }
-
 }
