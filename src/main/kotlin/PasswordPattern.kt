6ba941d4b06bd9fbafdabf16c1607533aa81fd19
David Isla <d.isla@ibermatica.com>
2022-08-11 18:41:46 +0200

    GREEN: Builder built :)

--- src/main/kotlin/PasswordPattern.kt
+++ src/main/kotlin/PasswordPattern.kt
@@ -1,9 +1,10 @@
-class PasswordPattern {
+class PasswordPattern private constructor(private val validations: List<Validation>) {
 
     companion object {
         fun validation(password: String): Boolean {
 
-            val validations = listOf( LengthValidation(),
+            val validations = listOf(
+                LengthValidation(),
                 UppercaseValidation(),
                 LowercaseValidation(),
                 DigitValidation(),
@@ -11,12 +12,47 @@ class PasswordPattern {
             )
 
             validations.forEach {
-                if( it(password) )
+                if (it(password))
                     return false
             }
 
             return true
         }
+    }
+
+    data class Builder(
+        var minimumLength: Int = 0,
+        var withUppercase: Boolean = false,
+        var withLowercase: Boolean = false,
+        var withDigit: Boolean = false,
+        var withUnderscore: Boolean = false ) {
+
+        fun withMinimumLength(minimum: Int) = apply { minimumLength = minimum }
+        fun withUppercase() = apply { withUppercase = true }
+        fun withLowercase() = apply { withLowercase = true }
+        fun withDigit() = apply { withDigit = true }
+        fun withUnderscore() = apply { withUnderscore = true }
+
+        fun build(): PasswordPattern {
+            val validations = mutableListOf<Validation>()
+
+            if (minimumLength > 0) validations.add(LengthValidation())
+            if (withUppercase) validations.add(UppercaseValidation())
+            if (withLowercase) validations.add(LowercaseValidation())
+            if (withDigit) validations.add(DigitValidation())
+            if (withUnderscore) validations.add(UnderscoreValidation())
+
+            return PasswordPattern(validations)
+        }
+    }
+
+    fun validate(password: String): Boolean {
+        validations.forEach {
+            if (it(password))
+                return false
+        }
+
+        return true
     }
 
 }
