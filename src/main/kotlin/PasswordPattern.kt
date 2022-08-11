682ed4d2625d63fa86e03b6cc279f7726b55e2f4
David Isla <d.isla@ibermatica.com>
2022-08-11 18:11:47 +0200

    REFACTOR: Restructured logic, to check out the password as multiple validations in a list

--- src/main/kotlin/PasswordPattern.kt
+++ src/main/kotlin/PasswordPattern.kt
@@ -2,33 +2,21 @@ class PasswordPattern {
 
     companion object {
         fun validation(password: String): Boolean {
-            if (checkLength(password))
-                return false
 
-            if (checkUppercase(password))
-                return false
+            val validations = listOf( LengthValidation(),
+                UppercaseValidation(),
+                LowercaseValidation(),
+                DigitValidation(),
+                UnderscoreValidation()
+            )
 
-            if (checkLowercase(password))
-                return false
-
-            if (checkDigit(password))
-                return false
-
-            if (checkUnderscore(password))
-                return false
+            validations.forEach {
+                if( it(password) )
+                    return false
+            }
 
             return true
         }
-
-        private fun checkUnderscore(password: String) = password.count { it == '_' } == 0
-
-        private fun checkDigit(password: String) = password.count { it.isDigit() } == 0
-
-        private fun checkLowercase(password: String) = password.count { it.isLowerCase() } == 0
-
-        private fun checkUppercase(password: String) = password.count { it.isUpperCase() } == 0
-
-        private fun checkLength(password: String) = password.length < 9
     }
 
 }
